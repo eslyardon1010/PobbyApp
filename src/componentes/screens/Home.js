b4bba232849e6e713cd369a5f7fb81a0f5b4c701
eslyardon1010 <eslyarsansu@gmail.com>
2021-06-25 11:50:27 -0600

    Agregacion de componentes

--- src/componentes/screens/Home.js
+++ src/componentes/screens/Home.js
@@ -1,10 +1,28 @@
 import * as React from 'react';
 import Header from "../appbarHeader"
+import { StyleSheet, View } from "react-native";
+import { Searchbar } from 'react-native-paper';
+import { Button } from 'react-native-paper';
+
 
 const Home = () => {
-    return ( <
-        Header / >
+  const [searchQuery, setSearchQuery] = React.useState('');
+   const onChangeSearch = query => setSearchQuery(query);
+return ( 
+  <View>
+        <Header/>
+    <Searchbar
+      placeholder="Search"
+      onChangeText={onChangeSearch}
+      value={searchQuery}
+    />
+    <Button icon="" mode="contained" onPress={() => console.log('Pressed')}>
+    Mascotas
+  </Button>
+   </View> 
+
     );
+    
 }
 
 export default Home;
